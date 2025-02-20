355edf434c4b29070c9cbefb4aa88d71df49bcc4
robindevilliers2 <robindevilliers2@gmail.com>
2025-02-20 13:45:08 +0200

    More styles - layout

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="text-sm font-semibold">Robin</div>;
+  return <div className="hidden text-sm font-semibold md:block">Robin</div>;
 }
 
 export default Username;
