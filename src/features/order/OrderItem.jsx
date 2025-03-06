1e5a6cb9fbacafd14edd4db7749358fad8cbd7a2
robindevilliers2 <robindevilliers2@gmail.com>
2025-03-06 18:06:03 +0200

    finishing touches

--- src/features/order/OrderItem.jsx
+++ src/features/order/OrderItem.jsx
@@ -2,16 +2,18 @@ import { formatCurrency } from "../../utils/helpers";
 
 function OrderItem({ item, isLoadingIngredients, ingredients }) {
   const { pizzaId, quantity, name, totalPrice } = item;
-  if (isLoadingIngredients && ingredients)
-    console.log(isLoadingIngredients, ingredients);
+
   return (
-    <li className="py-3" key={pizzaId}>
+    <li className="space-y-1 py-3" key={pizzaId}>
       <div className="flex items-center justify-between gap-4 text-sm">
         <p>
           <span className="font-bold">{quantity}&times;</span> {name}
         </p>
         <p className="font-bold">{formatCurrency(totalPrice)}</p>
       </div>
+      <p className="text-sm capitalize italic text-stone-500">
+        {isLoadingIngredients ? "Loading..." : ingredients.join(", ")}
+      </p>
     </li>
   );
 }
